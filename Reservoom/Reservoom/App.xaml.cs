a36d32b8deb5e3a521baa43144f8c8d70c52563f
ANUKUL OJHA <anukul289@gmail.com>
2024-05-01 13:29:54 +0530

    Model code added

--- Reservoom/Reservoom/App.xaml.cs
+++ Reservoom/Reservoom/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Reservoom.Exceptions;
+using Reservoom.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,28 @@ namespace Reservoom
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("Anukul Suites");
+            try
+            {
+                hotel.MakeReservation(new Reservation(
+                new RoomID(1, 3),
+                "Anukul",
+                new DateTime(2000, 1, 1),
+                new DateTime(2000, 1, 2)));
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 3),
+                    "Anukul",
+                    new DateTime(2000, 1, 1),
+                    new DateTime(2000, 1, 4)));
+            }catch(ReservationConflictException ex)
+            {
+
+            }
+            
+            IEnumerable<Reservation> reservation = hotel.GetReservationsForUser("Anukul");
+            base.OnStartup(e);
+        }
     }
 }
---
+++ Reservoom/Reservoom/Models/ReservationBook.cs
@@ -0,0 +1,34 @@
+﻿using Reservoom.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.Models
+{
+    public class ReservationBook
+    {
+        private readonly List<Reservation> _reservations;
+
+        public ReservationBook()
+        {
+            _reservations = new List<Reservation>();
+        }
+        public IEnumerable<Reservation> GetReservationsForUser(string username)
+        {
+            return _reservations.Where(x => x.Username == username);
+        }
+        public void AddReservation(Reservation reservation)
+        {
+            foreach(Reservation existingReservation in _reservations)
+            {
+                if (existingReservation.Conflicts(reservation))
+                {
+                    throw new ReservationConflictException(existingReservation, reservation);
+                }
+            }
+            _reservations.Add(reservation);
+        }
+    }
+}
